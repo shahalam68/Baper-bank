287fb65d282a90c0ae29d630e33948aaaa36d2a7
Soykot Alam <a.k.mshahalam68@gmail.com>
2022-06-29 01:15:43 +0600

    add deposit

--- js/banking.js
+++ js/banking.js
@@ -2,6 +2,14 @@
 document.getElementById('deposit-button').addEventListener('click',function(){
     // get deposit amount
     const depositInput = document.getElementById('deposit-amount');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const newDepositText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositText);
+    // console.log(depositAmount);
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText)
+    newDepositTotal = parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
+    depositTotal.innerText = newDepositTotal;
+    depositInput.value ='';
+    // update account balance
 });
